5097333ff8fb29ea648b81db7b42562237b5dc11
Emanijaz15 <eijaz7357@gmail.com>
2025-08-04 19:26:28 +0500

    changes

--- restaurant_project/settings.py
+++ restaurant_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-=t79a7ixx&hw02fv%m3-ky5l=x-&d=9k_9mht%rx-a1z*%g093
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
